1f7316ca4462efaf8209295c6dc87524612c5dc2
ALF188 <adam.felder88@gmail.com>
2023-03-29 16:26:13 +0000

    First commit

---
+++ frc-robot-template/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.*;
+import frc.robot.commands.*;
+import frc.robot.commands.autos.Autos;
+import frc.robot.subsystems.*;
+
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  //SmartDashboard autonomous selector
+  private SendableChooser<Command> autoChooser;
+
+  //Xbox Controller for command based 
+  private final CommandXboxController driverController =
+      new CommandXboxController(ControllerConstants.kDriverControllerPort);
+  
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  private void configureButtonBindings() {
+    
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    //return m_autoCommand;
+    return null;
+  }
+}
